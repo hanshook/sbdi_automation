5020ceaf2bdeb5f023bf5870396e5a3cc0f14523
Hans Höök <hans.hook@nrm.se>
2022-01-31 16:34:55 +0100

    Added backup restore and refactored backup logic - not tested

--- ansible/roles/backup_target/files/bin/create_backup_snapshot.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-zfs snapshot backup/current@snap_$(date +%Y%m%d_%H%M%S)
--- ansible/roles/backup_target/files/bin/mount_latest_snapshot.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-latest_snapshot=$(zfs list -t snapshot -o name -s creation | tail -1)
-
-zfs clone $latest_snapshot backup/latest
-#zfs destroy backup/latest
