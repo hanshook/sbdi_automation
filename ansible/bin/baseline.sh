9b28ee713ef20a9af69686517a80283a306da2ac
Hans Höök <hans.hook@nrm.se>
2022-02-11 14:44:44 +0100

    Install script for Nagios monitoring in place and some clean up

--- ansible/bin/baseline.sh
+++ ansible/bin/baseline.sh
@@ -10,4 +10,7 @@ ansible-playbook setup_admin_users.yml || exit 1
 ansible-playbook setup.yml || exit 1
 ansible-playbook docker_storage.yml
 ansible-playbook backup.yml
+ansible-playbook nagios_server.yml
+ansible-playbook nagios_monitoring.yml
+
 
