6acba3ea6ec3cf17a593b087c2d2e3ed85ead623
Hans Höök <hans.hook@nrm.se>
2022-02-15 08:56:42 +0100

    Added an -nrm switch to basline script in order to support deploy in NRM environemnt

--- ansible/bin/baseline.sh
+++ ansible/bin/baseline.sh
@@ -1,12 +1,43 @@
 #! /bin/bash
 
+# TBD: Add verbose output from Ansible!
+verbose=""
+
+# NRM blocks DNS, i.e. 1.1.1.1 is not accessile
+# It this scritp is run with option -nrm
+# This script will deploy using NRMs DNS servers
+# Note! It is unknown if this is acceptable because those
+# DNS servers will not be present in the cloud.
+
+nrm=false 
+
+while true 
+do
+    case $1 in
+	-v) verbose="-v"
+	    shift
+	    ;;
+	-nrm) nrm=true
+	      shift
+	      ;;
+	*) break	    
+	   ;;
+    esac
+done
+
 cd $(dirname $0)
 cd ..
 echo "Establish a basline of all host after setup"
-ansible-playbook deploy.yml || exit 1
+if $nrm
+then
+    ansible-playbook deploy_nrm.yml || exit 1
+else
+    ansible-playbook deploy.yml || exit 1
+fi
 ansible-playbook local_ssh.yml || exit 1
 ansible-playbook setup_admin_users.yml || exit 1
-# do not: ansible-playbook harden.yml
+# TODO: When to harden and how to reverse?
+# For the time beeing do not do: ansible-playbook harden.yml
 ansible-playbook setup.yml || exit 1
 ansible-playbook docker_storage.yml || exit 1
 ansible-playbook backup.yml || exit 1
