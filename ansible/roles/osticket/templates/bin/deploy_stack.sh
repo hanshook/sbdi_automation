7c783a8561260d4770670375bb8c89593ab34efc
Hans Höök <hans.hook@nrm.se>
2022-02-21 16:57:53 +0100

    Working on proxying

---
+++ ansible/roles/osticket/templates/bin/deploy_stack.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+cd $(dirname $0)
+{% if deployment_prefix is defined  %}
+. /opt/sbdi/lib/log_utils
+{% else %}
+. log_utils
+{% endif %}
+
+cd ..
+application_name=${PWD##*/}
+log_logging_application="MGM/${application_name}"
+
+export DOCKER_CTX={{ docker_ctx | default('/docker') }}
+
+cd ${DOCKER_CTX}/var/volumes
+for volume in {{ docker_volumes }} 
+do
+    if [ ! -d ${volume} ]
+    then
+	if sudo mkdir ${volume}
+	then
+	    log_info "Created empty volume directory ${volume}"
+	else
+	    log_fatal 2 "Failed to created empty volume directory ${volume}"
+	fi
+	    
+    fi
+done
+
+cd ${DOCKER_CTX}/etc/${application_name}
+
+export CURRENT_USER=$(id -u):$(id -g)
+
+log_info "Deploying stack ${application_name}"
+
+docker stack deploy --compose-file=docker-compose.yml ${application_name}
