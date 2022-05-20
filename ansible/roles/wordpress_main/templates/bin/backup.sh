17ce8230c05339cf4c2a9f15fd4271b7f69b5df1
Hans Höök <hans.hook@nrm.se>
2022-05-20 14:44:05 +0200

    Backup and restore wp scripts fro all sites

--- ansible/roles/wordpress_main/templates/bin/backup.sh
+++ ansible/roles/wordpress_main/templates/bin/backup.sh
@@ -1,26 +1,52 @@
 #! /bin/bash
 cd $(dirname $0)
-. /opt/sbdi/lib/log_utils
 
+# ================
+# Backup wordpress
+# ================
+
+USAGE="USAGE: $0 [-nodb] [-nofiles] [-to <backup name>] all | main | docs | tools"
+
+APPLICATION_PATH=${PWD%/*}
+APPLICATION_NAME=${APPLICATION_PATH##*/}
+# Currently the wordpress application is called worpress_main
+# This is expected to be wordpress (since main is a site)
+# Therefore remove the '_main' in APPLICATION_NAME
+# This will ensute the operation of this script even after
+# the aplication name has been changed
+BASE_APPLICATION_NAME="${APPLICATION_NAME%_*}" # -> grab everything up to first '_' 
+
+. /opt/sbdi/lib/log_utils
+log_logging_application="MGM/${APPLICATION_NAME}"
+
+switches=''
 backup_db=true
 backup_files=true
-backup=""
+backup_name=""
+
 while true 
 do
     case $1 in
 	-nodb)
 	    backup_db=false
+	    switches="${switches} -nodb"
 	    shift
 	    ;;
 	-nofiles)
 	    backup_files=false
+	    switches="${switches} -nofiles"
 	    shift
 	    ;;
 	-to)
-	    backup=$2
+	    backup_name=$2
+	    switches="${switches} -to $2"
 	    shift
 	    shift
-	    ;;		
+	    ;;
+	-h)
+	    echo $USAGE
+	    shift
+	    ;;
 	*) break	    
 	   ;;
     esac
@@ -28,33 +54,36 @@ done
 
 site=${1:-"main"}
 
-	  
-
 [ $EUID -eq 0 ] && log_fatal 88 "Do *not* run as root"
 if ! id -nG "$USER" | grep -qw "docker"
 then
     log_fatal 88 "User must belong to group 'docker'"
 fi
 
-cd ..
-application_name=${PWD##*/}
-log_logging_application="MGM/${application_name}"
-
-export DOCKER_CTX={{ docker_ctx | default('/docker') }}
-
 case $site in
     main)  ;;
     tools) ;;
     docs)  ;;
-    *) log_fatal 90 "site (${site} must be one of 'main', 'tools' or 'docs'" ;;
+    all)   ;;
+    *) log_fatal 90 "site (${site} must be one of 'all', 'main', 'tools' or 'docs'" ;;
 esac
 
-log_info "Will attempt to backup site ${site}"
+if [ "$site" == "all" ]
+then
+    log_info "Will attempt to backup all sites"
 
-base_application_name="${application_name%_*}" # '_main' in application_name
-                                               # -> grab everything up to first '_' 
-env_file_name=".env${base_application_name}_${site}"  
-env_file="${DOCKER_CTX}/etc/${application_name}/env/${env_file_name}"
+    for site in main tools docs
+    do
+	$0 $switches $site
+    done
+else
+    log_info "Will attempt to backup site ${site}"
+fi
+
+export DOCKER_CTX={{ docker_ctx | default('/docker') }}
+
+env_file_name=".env${BASE_APPLICATION_NAME}_${site}"  
+env_file="${DOCKER_CTX}/etc/${APPLICATION_NAME}/env/${env_file_name}"
 
 [ ! -e "${env_file}" ] && log_fatal 91 "env file does not exist: ${env_file}" 
 
@@ -69,33 +98,33 @@ MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 [ -z "$MYSQL_ROOT_PASSWORD" ] &&  log_fatal 94 "MYSQL_ROOT_PASSWORD not in env file" 
 
 
-export BACKUP_CTX=${DOCKER_CTX}/var/backup/${application_name}
+export BACKUP_CTX=${DOCKER_CTX}/var/backup/${APPLICATION_NAME}
 
 [ ! -d ${BACKUP_CTX} ] && log_fatal 95 "No backup context found: ${BACKUP_CTX}" 
 
 cd ${BACKUP_CTX}
 
-if [ ! -z "${backup}" ]
+if [ ! -z "${backup_name}" ]
 then
-    if [ -e ${backup} ]
+    if [ -e ${backup_name} ]
     then
-	log_warn "Backup ${backup} already exists - will be backed up"
+	log_warn "A backup with name ${backup_name} already exists"
     fi
     NEW_BACKUP_DIR=`mktemp -d -p .`
     if chgrp docker ${NEW_BACKUP_DIR} && chmod 770 ${NEW_BACKUP_DIR}
     then
-	if mv --backup=numbered -T ${NEW_BACKUP_DIR} ${backup}
+	if mv --backup=numbered -T ${NEW_BACKUP_DIR} ${backup_name}
 	then
-	    cd ${backup}
+	    cd ${backup_name}
 	else
 	    rm -rf ${NEW_BACKUP_DIR}
-	    log_fatal 96 "Unable to create backup directory: ${backup}, mv --backup=numbered -T ${NEW_BACKUP_DIR} ${backup}, error: $?"
+	    log_fatal 96 "Unable to create backup directory: ${backup_name}, mv --backup=numbered -T ${NEW_BACKUP_DIR} ${backup_name}, error: $?"
 	fi
     else
 	rm -rf ${NEW_BACKUP_DIR}
-	log_fatal 96 "Unable to create backup directory: ${backup}, chgrp docker ${NEW_BACKUP_DIR} && chmod 770 ${NEW_BACKUP_DIR}, error: $?"
+	log_fatal 96 "Unable to create backup directory: ${backup_name}, chgrp docker ${NEW_BACKUP_DIR} && chmod 770 ${NEW_BACKUP_DIR}, error: $?"
     fi
-    log_info "Using backup location: $(pwd)"
+    log_info "Using named backup location: $(pwd)"
 else
     log_info "Using standard backup location: $(pwd)"
 fi
@@ -143,8 +172,7 @@ else
     ARTEFACT=${site}-wordpress-db.sql
     ARTEFACT_NAME="database dump"
 
-    SERVICE_NAME="${application_name}_${MYSQL_HOST}"
-#    SERVICE_NAME="${application_name%_*}_${MYSQL_HOST}"
+    SERVICE_NAME="${APPLICATION_NAME}_${MYSQL_HOST}"
 
     log_info "Creating ${ARTEFACT_NAME} from ${SERVICE_NAME} and saving it as $(pwd)/new_${ARTEFACT}"
     if /opt/sbdi/bin/service_exec $SERVICE_NAME mysqldump --user root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE > new_${ARTEFACT}
@@ -173,8 +201,7 @@ else
     ARTEFACT=${BACKUP_TARBALL_NAME}.tgz
     ARTEFACT_NAME="/var/www/html tarball"
 
-    SERVICE_NAME="${application_name}_${WORDPRESS_HOST}"
-    #    SERVICE_NAME="${application_name%_*}_${WORDPRESS_HOST}"
+    SERVICE_NAME="${APPLICATION_NAME}_${WORDPRESS_HOST}"
     
     log_info "Creating ${ARTEFACT_NAME} from ${SERVICE_NAME} and saving it as $(pwd)/new_${ARTEFACT}"
     if /opt/sbdi/bin/service_exec -i $SERVICE_NAME tar cz /var/www/html > new_${ARTEFACT}
--- ansible/roles/wordpress_main/templates/bin/restore_backup.sh
+++ ansible/roles/wordpress_main/templates/bin/restore_backup.sh
@@ -1,26 +1,51 @@
 #! /bin/bash
 cd $(dirname $0)
-. /opt/sbdi/lib/log_utils
 
+# ========================
+# Restore wordpress backup
+# ========================
+
+USAGE="USAGE: $0 [-nodb] [-nofiles] [-from <backup name>] all | main | docs | tools"
+
+APPLICATION_PATH=${PWD%/*}
+APPLICATION_NAME=${APPLICATION_PATH##*/}
+# Currently the wordpress application is called worpress_main
+# This is expected to be wordpress (since main is a site)
+# Therefore remove the '_main' in APPLICATION_NAME
+# This will ensute the operation of this script even after
+# the aplication name has been changed
+BASE_APPLICATION_NAME="${APPLICATION_NAME%_*}" # -> grab everything up to first '_'
+
+. /opt/sbdi/lib/log_utils
+log_logging_application="MGM/${APPLICATION_NAME}"
+
+switches=''
 restore_db=true
 restore_files=true
-backup=""
+backup_name=""
 while true 
 do
     case $1 in
 	-nodb)
 	    restore_db=false
+	    switches="${switches} -nodb"
 	    shift
 	    ;;
 	-nofiles)
 	    restore_files=false
+	    switches="${switches} -nofiles"
 	    shift
 	    ;;
 	-from)
-	    backup=$2
+	    backup_name=$2
+	    switches="${switches} -from $2"
 	    shift
 	    shift
-	    ;;	
+	    ;;
+	-h)
+	    echo $USAGE
+	    shift
+	    ;;
 	*) break	    
 	   ;;
     esac
@@ -28,33 +53,36 @@ done
 
 site=${1:-"main"}
 
-	  
-
 [ $EUID -eq 0 ] && log_fatal 88 "Do *not* run as root"
 if ! id -nG "$USER" | grep -qw "docker"
 then
     log_fatal 88 "User must belong to group 'docker'"
 fi
 
-cd ..
-application_name=${PWD##*/}
-log_logging_application="MGM/${application_name}"
-
-export DOCKER_CTX={{ docker_ctx | default('/docker') }}
-
 case $site in
     main) ;;
     tools) ;;
     docs) ;;
+    all)   ;;
     *) log_fatal 90 "site (${site} must be one of 'main', 'tools' or 'docs'" ;;
 esac
 
-log_info "Will attempt to restore backup for site ${site}"
+if [ "$site" == "all" ]
+then
+    log_info "Will attempt to restore bakup for all sites"
 
-base_application_name="${application_name%_*}" # '_main' in application_name
-                                               # -> grab everything up to first '_' 
-env_file_name=".env${base_application_name}_${site}"  
-env_file="${DOCKER_CTX}/etc/${application_name}/env/${env_file_name}"
+    for site in main tools docs
+    do
+	$0 $switches $site
+    done
+else
+    log_info "Will attempt to restore backup for site ${site}" 
+fi
+
+export DOCKER_CTX={{ docker_ctx | default('/docker') }}
+
+env_file_name=".env${BASE_APPLICATION_NAME}_${site}"  
+env_file="${DOCKER_CTX}/etc/${APPLICATION_NAME}/env/${env_file_name}"
 
 [ ! -e "${env_file}" ] && log_fatal 91 "env file does not exist: ${env_file}" 
 
@@ -69,17 +97,17 @@ MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 [ -z "$MYSQL_ROOT_PASSWORD" ] &&  log_fatal 94 "MYSQL_ROOT_PASSWORD not in env file" 
 
 
-export BACKUP_CTX=${DOCKER_CTX}/var/backup/${application_name}
+export BACKUP_CTX=${DOCKER_CTX}/var/backup/${APPLICATION_NAME}
 
 [ ! -d ${BACKUP_CTX} ] && log_fatal 95 "No backup context found: ${BACKUP_CTX}" 
 
 cd ${BACKUP_CTX}
 
-if [ ! -z "${backup}" ]
+if [ ! -z "${backup_name}" ]
 then
-    [ ! -d ${backup} ] && log_fatal 96 "No backup found: ${BACKUP_CTX}/${backup}"
-    cd ${backup}
-    log_info "Selected backup ${backup}"
+    [ ! -d ${backup_name} ] && log_fatal 96 "No backup found: ${BACKUP_CTX}/${backup_name}"
+    cd ${backup_name}
+    log_info "Selected backup ${backup_name}"
 else
     log_info "Select standard backup"
 fi
@@ -95,8 +123,7 @@ else
     ARTEFACT=${site}-wordpress-db.sql
     ARTEFACT_NAME="database dump"
 
-    SERVICE_NAME="${application_name}_${MYSQL_HOST}"
-    #    SERVICE_NAME="${application_name%_*}_${MYSQL_HOST}"
+    SERVICE_NAME="${APPLICATION_NAME}_${MYSQL_HOST}"
 
     [ ! -e "${ARTEFACT}" ] &&  log_fatal 96 "${ARTEFACT_NAME} $(pwd)/${ARTEFACT} does not exist"
     
@@ -123,8 +150,7 @@ else
     ARTEFACT=${BACKUP_TARBALL_NAME}.tgz
     ARTEFACT_NAME="/var/www/html tarball"
 
-    SERVICE_NAME="${application_name}_${WORDPRESS_HOST}"
-    #    SERVICE_NAME="${application_name%_*}_${WORDPRESS_HOST}"
+    SERVICE_NAME="${APPLICATION_NAME}_${WORDPRESS_HOST}"
 
     [ ! -e "${ARTEFACT}" ] &&  log_fatal 96 "${ARTEFACT_NAME} $(pwd)/${ARTEFACT} does not exist"
 
