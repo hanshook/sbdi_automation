fcdb56d93767bec42cb8136ea8bae5f36e0f9a3b
Hans Höök <hans.hook@nrm.se>
2022-08-26 15:48:33 +0200

    Fixed utility script minor typo

--- ansible/bin/deploy_docker_app.sh
+++ ansible/bin/deploy_docker_app.sh
@@ -4,7 +4,7 @@
 
 cd $(dirname $0)
 cd ..
-ansible-playbook docker_apps --extra-vars "only_app=$1" --skip-tags "static_html, haproxy"
+ansible-playbook docker_apps.yml --extra-vars "only_app=$1" --skip-tags "static_html, haproxy"
 
 
 
